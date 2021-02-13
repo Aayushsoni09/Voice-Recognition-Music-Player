4be939f176c6eded7a0bbf5ee139869752b15b31
Aayushsoni09 <44709621+Aayushsoni09@users.noreply.github.com>
2021-02-13 17:57:11 +0530

    Voice Recognizable Music Player

---
+++ playlist.js
@@ -0,0 +1,16 @@
+var data = {
+    "songs": [
+      {
+        "fileName": "rideit.mp3",
+        "singer": "Jay Sean",
+        "songName": "Ride It",
+        "cover":"rideit.jpg"
+      },
+      {
+        "fileName": "tummile.mp3",
+        "singer": "Neeraj Shridhar",
+        "songName": "Tum Mile",
+        "cover":"tummile.jpg"
+      },
+    ]
+  };
